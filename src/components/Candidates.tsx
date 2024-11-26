cb87c2c4e76c9e0bcaad627fa1440beaa7ed2cbc
mihaiconstantin <mihai@mihaiconstantin.com>
2024-11-26 15:12:18 +0100

    Feat: add `Summary` component

--- src/components/Candidates.tsx
+++ src/components/Candidates.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Summary from "./Summary";
 import QualifiedCandidate from "./QualifiedCandidate";
 import DropoutCandidate from "./DropoutCandidate";
 import { useData } from "../hooks/useData";
@@ -8,7 +9,7 @@ const Candidates: React.FC = () => {
     const data = useData();
 
     // Extract relevant information from the data array.
-    const { first, second, dropouts, statistics } = data;
+    const { first, second, dropouts } = data;
 
     // Construct the array of qualified candidates.
     const qualified = [first, second];
@@ -16,29 +17,8 @@ const Candidates: React.FC = () => {
     return (
         <section id="controls" className="row">
             <div className="col-12 shadow controls-wrapper">
-                <div className="row control">
-                    <div className="section-title">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="30"
-                            height="30"
-                            fill="currentColor"
-                            className="bi bi-bar-chart-steps"
-                            viewBox="0 0 16 16"
-                        >
-                            <path d="M.5 0a.5.5 0 0 1 .5.5v15a.5.5 0 0 1-1 0V.5A.5.5 0 0 1 .5 0M2 1.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zm2 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1-.5-.5zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5z" />
-                        </svg>
-                        <span>Împarte Voturile</span>
-                    </div>
-
-                    <div className="section-description">
-                        Pe baza ultimelor date de la ROAEP, <span className="total-votes-round-one">{statistics.roundOne}</span> persoane
-                        au votat în primul tur din {statistics.eligible} câte erau eligibile. În urma acestui vot,
-                        iar candidații <span className="name">{first.name}</span> și <span className="name">{second.name}</span> vor
-                        fi selectați pentru turul doi. Alege mai jos cum preconizezi că se vor împărți voturile în turul doi
-                        pentru fiecare candidat în parte.
-                    </div>
-                </div>
+                {/* Render the summary. */}
+                <Summary />
 
                 <div className="b row section-subtitle">
                     <div className="col-12">Candidații Din Turul Doi</div>
@@ -60,7 +40,6 @@ const Candidates: React.FC = () => {
                 </div>
 
                 {/* Render the dropouts. */}
-
                 {dropouts.map((candidate) => (
                     <DropoutCandidate
                         key={candidate.id}
---
+++ src/components/Summary.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useData } from "../hooks/useData";
+
+const Summary: React.FC = () => {
+    // Extract relevant information from the data array.
+    const { first, second, statistics } = useData();;
+
+    return (
+        <div className="row control">
+            <div className="section-title">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    fill="currentColor"
+                    className="bi bi-bar-chart-steps"
+                    viewBox="0 0 16 16"
+                >
+                    <path d="M.5 0a.5.5 0 0 1 .5.5v15a.5.5 0 0 1-1 0V.5A.5.5 0 0 1 .5 0M2 1.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zm2 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1-.5-.5zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5z" />
+                </svg>
+                <span>Împarte Voturile</span>
+            </div>
+
+            <div className="section-description">
+                Pe baza ultimelor date de la ROAEP, <span className="total-votes-round-one">{statistics.roundOne}</span> persoane
+                au votat în primul tur din {statistics.eligible} câte erau eligibile. În urma acestui vot,
+                candidații <span className="name">{first.name}</span> și <span className="name">{second.name}</span> vor
+                fi selectați pentru turul doi. Alege mai jos cum preconizezi că se vor împărți voturile în turul doi
+                pentru fiecare candidat în parte.
+            </div>
+        </div>
+    );
+};
+
+export default Summary;
