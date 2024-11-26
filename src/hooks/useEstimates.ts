adc7a41a6b4cc6cc196a1185fd1f57be89104788
mihaiconstantin <mihai@mihaiconstantin.com>
2024-11-26 11:45:35 +0100

    Fix: error in logic for qualified candidates in `useEstimates`

--- src/hooks/useEstimates.ts
+++ src/hooks/useEstimates.ts
@@ -1,23 +1,11 @@
 import { useMemo } from "react";
 import { useData } from "./useData";
+import { CandidateProps } from "../types/CandidateProps";
+import { InputProps } from "../types/InputProps";
 
-// Define the candidate interface.
-interface Candidate {
-    id: string;
-    name: string;
-    votes: number;
-    percentage: number;
-}
-
-// Define the input interface (i.e., the input from the user).
-interface Input {
-    from: string;
-    to: string;
-    value: number;
-}
 
 // Distribute the votes accordingly.
-export const useEstimates = (input: Input) => {
+export const useEstimates = (input: InputProps) => {
     // Get the data.
     const data = useData();
 
@@ -31,7 +19,7 @@ export const useEstimates = (input: Input) => {
     const value = input.value / 100;
 
     // Define the array of candidates.
-    const candidates: Candidate[] = [first, second, ...dropouts];
+    const candidates: CandidateProps[] = [first, second, ...dropouts];
 
     // Calculate the votes.
     const [votesFirstCandidate, votesSecondCandidate] = useMemo(() => {
@@ -39,45 +27,52 @@ export const useEstimates = (input: Input) => {
         let newVotesFirstCandidate = 0;
         let newVotesSecondCandidate = 0;
 
-        // If the interface candidate is the first qualified candidate.
-        if (from === first.id) {
-            // Distribute its own votes.
-            newVotesFirstCandidate = first.votes * value;
-            newVotesSecondCandidate = first.votes * (1 - value);
-        }
+        // If the input is coming from a qualified candidate.
+        if (from === to) {
+            // If the interface candidate is the first qualified candidate.
+            if (from === first.id) {
+                // Distribute its own votes.
+                newVotesFirstCandidate = first.votes * value;
+                newVotesSecondCandidate = first.votes * (1 - value);
+            }
 
-        // If the interface candidate is the second qualified candidate.
-        if (from === second.id) {
-            // Distribute its own votes.
-            newVotesFirstCandidate = second.votes * (1 - value);
-            newVotesSecondCandidate = second.votes * value;
-        }
+            // If the interface candidate is the second qualified candidate.
+            if (from === second.id) {
+                // Distribute its own votes.
+                newVotesFirstCandidate = second.votes * (1 - value);
+                newVotesSecondCandidate = second.votes * value;
+            }
+        // Otherwise, search for the candidate.
+        } else {
+            // Otherwise, search for the candidate in the array.
+            for (const candidate of candidates) {
+                // If the candidate in the array matches the giver from the input.
+                if (from === candidate.id) {
+                    // If the target is the first qualified candidate.
+                    if (to === first.id) {
+                        // Distribute the votes.
+                        newVotesFirstCandidate = first.votes + candidate.votes * value;
+                        newVotesSecondCandidate = second.votes + candidate.votes * (1 - value);
+                    }
 
-        // Otherwise, search for the candidate in the array.
-        for (const candidate of candidates) {
-            // If the candidate in the array matches the giver from the input.
-            if (from === candidate.id) {
-                // If the target is the first qualified candidate.
-                if (to === first.id) {
-                    // Distribute the votes.
-                    newVotesFirstCandidate = first.votes + candidate.votes * value;
-                    newVotesSecondCandidate = second.votes + candidate.votes * (1 - value);
+                    // If the target is the second qualified candidate.
+                    if (to === second.id) {
+                        // Distribute the votes.
+                        newVotesFirstCandidate = first.votes + candidate.votes * (1 - value);
+                        newVotesSecondCandidate = second.votes + candidate.votes * value;
+                    }
+
+                    // Break free once the candidate is found and processed.
+                    break;
                 }
-
-                // If the target is the second qualified candidate.
-                if (to === second.id) {
-                    // Distribute the votes.
-                    newVotesFirstCandidate = first.votes + candidate.votes * (1 - value);
-                    newVotesSecondCandidate = second.votes + candidate.votes * value;
-                }
-
-                // Break free once the candidate is found and processed.
-                break;
             }
         }
 
         // Return the votes.
-        return [newVotesFirstCandidate, newVotesSecondCandidate];
+        return [
+            Math.round(newVotesFirstCandidate),
+            Math.round(newVotesSecondCandidate)
+        ];
 
     }, [from, to, value, candidates]);
 
