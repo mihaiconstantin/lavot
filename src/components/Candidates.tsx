0b744a14717b15ade523e8ad2a532deb5c3769ad
mihaiconstantin <mihai@mihaiconstantin.com>
2024-11-27 11:44:17 +0100

    Design: add ribbons to controls subsections

--- src/components/Candidates.tsx
+++ src/components/Candidates.tsx
@@ -22,7 +22,12 @@ const Candidates: React.FC = () => {
                 <Summary />
 
                 <div className="b row text-muted display-6 section-subtitle">
-                    <div className="col-12">Candidații Din Turul Doi</div>
+                    <div className="col-12">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-fill" viewBox="0 0 16 16">
+                            <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
+                        </svg>
+                        <span>Candidații Din Turul Doi</span>
+                    </div>
                 </div>
 
                 {/* Render the qualified candidates. */}
@@ -37,7 +42,12 @@ const Candidates: React.FC = () => {
                 ))}
 
                 <div className="b row text-muted lead section-subtitle">
-                    <div className="col-12">Candidații Eliminați</div>
+                    <div className="col-12">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-fill" viewBox="0 0 16 16">
+                            <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
+                        </svg>
+                        <span>Candidații Eliminați</span>
+                    </div>
                 </div>
 
                 {/* Render the dropouts. */}
