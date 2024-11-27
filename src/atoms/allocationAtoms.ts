ffffdf934d05e667cb58e3bdd3a0589d85b5d8a3
mihaiconstantin <mihai@mihaiconstantin.com>
2024-11-27 21:10:35 +0100

    Feat: add global state for round two votes

--- src/atoms/allocationAtoms.ts
+++ src/atoms/allocationAtoms.ts
@@ -1,8 +1,9 @@
 import { atom } from "jotai";
 import { InputProps } from "../types/InputProps";
 import { data } from "../data/data";
-import { calculateVotes, initializeAllocations } from "../utils/calculateVotes";
+import { calculateVotes, initializeAllocations, initializeNewVoters } from "../utils/calculateVotes";
 import { CandidateProps } from "../types/CandidateProps";
+import { NewVoters } from "../types/NewVoters";
 
 
 // Define the votes atom with initial allocations.
@@ -17,5 +18,14 @@ export const allocationsAtom = atom<InputProps[]>(
 // Define the calculation derived atom with the initial value.
 export const votesAtom = atom<CandidateProps[]>(
     // Calculate the votes based on the initial allocations.
-    (get) => calculateVotes(data.first, data.second, data.dropouts, get(allocationsAtom))
+    (get) => calculateVotes(
+        data.statistics, data.first, data.second, data.dropouts,
+        get(allocationsAtom), get(newVotersAtom)
+    )
 );
+
+// Define the new votes atom with the initial value.
+export const newVotersAtom = atom<NewVoters>(
+    // Initialize the new votes based on the data.
+    initializeNewVoters(data.second, data.statistics)
+)
