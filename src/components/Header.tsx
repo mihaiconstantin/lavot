016c4791dc0f563fafcb4b2c08857e06726a51bc
mihaiconstantin <mihai@mihaiconstantin.com>
2024-11-27 04:23:07 +0100

    Feat: improve header text and typography

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,14 @@ const Header: React.FC = () => {
     return (
         <header className="row text-center">
             <div className="col-12">
-                <h1 className="display-6">
+                <h1 className="display-1 fw-lighter">
                     La <span className="flag-blue">V</span>
                     <span className="flag-yellow">O</span>
                     <span className="flag-red">T</span>
                 </h1>
-                <h3 className="display-6 text-muted">
-                    Romania <span className="text-underscore">Mai Poate</span>{" "}
-                    Decide
-                </h3>
+                <div className="header-subtitle display-6 fw-lighter text-secondary">
+                    Romania <span className="text-underscore">Mai Poate</span> Decide
+                </div>
             </div>
         </header>
     );
